d78fe8bab696fc380b6a361bade73ab49ff0ed8b
ajporterfield@gmail.com <ajporterfield@gmail.com@18c9a15c-d092-11de-bb02-e302c5db49d5>
2012-09-15 17:42:42 +0000

    Cleaning up old, unused branches

--- branches/modular-elements/includes/php-email-address-validation/ajax-handler.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("EmailAddressValidator.php");
-$emailObj = new EmailAddressValidator;
-if(!$emailObj->check_email_address($_POST["email"]))
-	echo str_replace("[LABEL]", $_POST["label"], $_POST["format"]);
-
-?>
